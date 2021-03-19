b38275da0c123803d56b34be75601eb40ff69e4b
Alejandro Villar Rubio <avr-98@hotmail.es>
2021-03-19 14:01:25 +0100

    Aumenta y disminuye el tamaño de la fuente del TEXTO DE EJEMPLO que hay en la pantalla de PRESETTINGS.

--- android-app/app/src/main/java/es/ucm/bany/PreSettingFragment.java
+++ android-app/app/src/main/java/es/ucm/bany/PreSettingFragment.java
@@ -1,16 +1,28 @@
 package es.ucm.bany;
 
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import org.w3c.dom.Text;
+
 public class PreSettingFragment extends Fragment {
+
+    Button _button_presseting_theme_light,
+            _button_presseting_theme_dark,
+            _button_presseting_theme_pascal,
+            _button_presseting_font_max,
+            _button_presseting_font_min;
+
+    TextView _text_presseting_example;
 
     @Override
     public View onCreateView(
@@ -24,43 +36,47 @@ public class PreSettingFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        view.findViewById(R.id.button_presseting_theme_light).setOnClickListener(new View.OnClickListener() {
+        _button_presseting_theme_light = (Button) view.findViewById(R.id.button_presseting_theme_light);
+        _button_presseting_theme_dark = (Button) view.findViewById(R.id.button_presseting_theme_dark);
+        _button_presseting_theme_pascal = (Button) view.findViewById(R.id.button_presseting_theme_pascal);
+
+        _button_presseting_font_max = (Button) view.findViewById(R.id.button_presseting_font_max);
+        _button_presseting_font_min = (Button) view.findViewById(R.id.button_presseting_font_min);
+        _text_presseting_example = (TextView) view.findViewById(R.id.text_presseting_example);
+
+
+        _button_presseting_theme_light.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Utils.changeToTheme(getActivity(), Utils.THEME_DEFAULT);
             }
         });
 
-        view.findViewById(R.id.button_presseting_theme_dark).setOnClickListener(new View.OnClickListener() {
+        _button_presseting_theme_dark.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Utils.changeToTheme(getActivity(), Utils.THEME_DARK);
             }
         });
 
-        view.findViewById(R.id.button_presseting_theme_pascal).setOnClickListener(new View.OnClickListener() {
+        _button_presseting_theme_pascal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Utils.changeToTheme(getActivity(), Utils.THEME_PASCAL);
             }
         });
 
-        view.findViewById(R.id.button_presseting_font_max).setOnClickListener(new View.OnClickListener() {
+        _button_presseting_font_max.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView text = (TextView) view.findViewById(R.id.text_presseting_example);
-                System.out.println(text);
-                //text.setText("HOLA");
-                //text.setTextSize(text.getTextSize() + 2);
+                _text_presseting_example.setTextSize(TypedValue.COMPLEX_UNIT_PX, _text_presseting_example.getTextSize() + 8);
             }
         });
 
-        view.findViewById(R.id.button_presseting_font_min).setOnClickListener(new View.OnClickListener() {
+        _button_presseting_font_min.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                TextView text = (TextView) view.findViewById(R.id.text_presseting_example);
-                text.setText("ADIOS");
-                //text.setTextSize(text.getTextSize() - 2);
+                _text_presseting_example.setTextSize(TypedValue.COMPLEX_UNIT_PX, _text_presseting_example.getTextSize() - 8);
             }
         });
     }
